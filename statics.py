81cc54ebedde5fb6d3ef7333db8e12cad6a2a6ec
Frederic Marazzato <marazzato@lsu.edu>
2022-03-05 09:34:37 -0600

    Added the nonlinear term in the statics computation. Check what happens in the dynamic one.

--- statics.py
+++ statics.py
@@ -7,7 +7,7 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import sys
 
-alpha = 0 #alpha \in [0,1)
+alpha = 0.99 #alpha \in [0,1)
 
 size = 20
 mesh = UnitIntervalMesh(size)
@@ -23,9 +23,9 @@ U = VectorFunctionSpace(mesh, 'CG', 1, dim=2)
 u = Function(U, name="displacement")
 v = TestFunction(U)
 du = TrialFunction(U)
-energy = 0.5 * (1 - alpha) * inner(u[0].dx(0), u[0].dx(0)) * dx + 0.5 * inner(u[1].dx(0), u[1].dx(0)) * dx
+energy = 0.5 * (1 - alpha) * u[0].dx(0) * u[0].dx(0) * dx + 0.5 * u[1].dx(0) * u[1].dx(0) * dx
+energy -= alpha * (sqrt(u[0].dx(0)**2 + (1 + u[1].dx(0))**2) - 1 - u[1].dx(0)) * dx #nonlinear term
 a = derivative(energy,u,v)
-a = replace(a,{u:du})
 
 #linear form
 l = Constant(1) * v[1] * dx
@@ -37,9 +37,14 @@ def bnd(x, on_boundary):
 #Boundary conditions
 bc = DirichletBC(U, Constant((0,0)), bnd)
 
-#Solving the problem
-solve(a == l, u, bc)
+##Linear solve
+#a = replace(a,{u:du})
+#solve(a == l, u, bc)
 
-##Plot the solution
-#plot(u[1])
-#plt.show()
+#Nonlinear solve$
+F = a - l
+solve(F == 0, u, bc)
+
+#Plot the solution
+plot(u[1])
+plt.show()
